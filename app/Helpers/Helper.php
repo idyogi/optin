0909a3cd9a38d62e28fdea1b1fb114f0d5a3ffd1
idyogi <yogyan12@gmail.com>
2022-12-13 20:02:26 +0700

    https bug

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -1,9 +1,9 @@
 <?php
 function base_url(): string
 {
-    $secure_connection = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on';
+//    $secure_connection = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on';
 
-    $protocol = $secure_connection ? 'https://' : 'http://';
+    $protocol = 'https://';
     return $protocol . $_SERVER['HTTP_HOST'];
 }
 
