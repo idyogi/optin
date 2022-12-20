e60f18aaa41347732965437ee071d444eb1d98e5
idyogi <yogyan12@gmail.com>
2022-12-21 04:58:40 +0700

    hapus yg gk fungsi

--- resources/js/Pages/Panel/Forms/ResponseFields/ResponseFields.jsx
+++ resources/js/Pages/Panel/Forms/ResponseFields/ResponseFields.jsx
@@ -63,34 +63,8 @@ function ResponseFields({responseFields, transform, formatFields,submissionId, i
             <div className="py-3 px-6 bg-gray-200 border-t-1 border-gray-300">
                 <div className="mt-2">Standar Response</div>
                 <ButtonInput name={"text"} type={'custom_html'} callback={addFieldList}/>
-
-                <button
-                    className="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline mt-1 mr-1 bg-gray-600 text-white hover:bg-gray-700">countdown
-                </button>
-                <button
-                    className="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline mt-1 mr-1 bg-gray-600 text-white hover:bg-gray-700">konten
-                    terkunci
-                </button>
-                <button
-                    className="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline mt-1 mr-1 bg-gray-600 text-white hover:bg-gray-700">text
-                    by status
-                </button>
                 <div className="mt-2">Viral Response</div>
                 <ButtonInput name={"share"} type={'share'} callback={addFieldList}/>
-
-                <button
-                    className="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline mt-1 mr-1 bg-gray-600 text-white hover:bg-gray-700">click
-                </button>
-                <button
-                    className="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline mt-1 mr-1 bg-gray-600 text-white hover:bg-gray-700">link
-                    rotator
-                </button>
-                <button
-                    className="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline mt-1 mr-1 bg-gray-600 text-white hover:bg-gray-700">point
-                </button>
-                <button
-                    className="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline mt-1 mr-1 bg-gray-600 text-white hover:bg-gray-700">leaderboard
-                </button>
             </div>)}
         </div>)
 }
