be6f86970ef7a4109d4d6bfa8e50dd916bbb5980
idyogi <yogyan12@gmail.com>
2022-12-21 10:44:39 +0700

    section if no form

--- resources/js/Pages/Panel/Forms/Index.jsx
+++ resources/js/Pages/Panel/Forms/Index.jsx
@@ -6,11 +6,25 @@ function IndexForm({forms, config}) {
 
     return (
         <PanelLayout title={'List Form'} subTitle={''} trailing={(<a disabled="false"
-                                                                 className="inline-flex items-center justify-center text-sm font-medium transition-all ease-in-out duration-100 focus:outline-none focus:ring border rounded-md border-primary-500 bg-primary-500 text-white shadow hover:bg-primary-400 hover:border-primary-400 focus:border-primary-700 focus:bg-primary-600 px-3 py-2 text-sm"
-                                                                 target="_self"
-                                                                 href="/panel/forms/create"> Create
-            Tambah Form </a>)}>
-
+                                                                     className="inline-flex items-center justify-center text-sm font-medium transition-all ease-in-out duration-100 focus:outline-none focus:ring border rounded-md border-primary-500 bg-primary-500 text-white shadow hover:bg-primary-400 hover:border-primary-400 focus:border-primary-700 focus:bg-primary-600 px-3 py-2 text-sm"
+                                                                     target="_self"
+                                                                     href="/panel/forms/create"> Create Form </a>)}>
+            {forms.length === 0 && (
+                <div className="p-8">
+                    <div
+                        className="text-left relative mx-auto flex max-w-md flex-col items-center justify-center space-y-6 rounded-lg border p-6 dark:border-gray-600">
+                        <div
+                            className="absolute top-0 right-0 -mt-5 -mr-5 flex h-12 w-12 items-center justify-center rounded-lg bg-white shadow dark:bg-gray-500">
+                            <i className="fa text-primary-500 text-xl dark:text-gray-300 fa-globe"></i></div>
+                        <header className="max-w-xs space-y-2"><h2
+                            className="text-xl font-semibold tracking-tight dark:text-gray-400">You don't have any forms
+                            yet</h2><p className="text-md font-medium text-gray-500 dark:text-gray-400">Currently you do
+                            not own
+                            any form. Luckily Funnel.ink makes this real easy for you to do.</p></header>
+                        <a disabled="false"
+                           className="inline-flex items-center justify-center text-sm font-medium transition-all ease-in-out duration-100 focus:outline-none focus:ring border rounded-md border-primary-500 bg-primary-500 text-white shadow hover:bg-primary-400 hover:border-primary-400 focus:border-primary-700 focus:bg-primary-600 px-3 py-2 text-sm"
+                           target="_self" href="/panel/forms/create">Create form</a></div>
+                </div>)}
             <div className="min-h-screen">
                 <div className="container mx-auto px-6">
                     <div>
@@ -92,6 +106,7 @@ function IndexForm({forms, config}) {
                     </div>
                 </div>
             </div>
+
         </PanelLayout>
     );
 }
