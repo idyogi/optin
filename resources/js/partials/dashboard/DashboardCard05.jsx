88997e0df6c2de4e4ffa6973424524e779e8aa98
idyogi <yogyan12@gmail.com>
2023-01-07 03:25:34 +0700

    bug graph

--- resources/js/partials/dashboard/DashboardCard05.jsx
+++ resources/js/partials/dashboard/DashboardCard05.jsx
@@ -98,10 +98,6 @@ function DashboardCard05({lead}) {
             className="flex flex-col col-span-full sm:col-span-6 bg-white shadow-lg rounded-sm border border-slate-200">
             <header className="px-5 py-4 border-b border-slate-100 flex items-center">
                 <h2 className="font-semibold text-slate-800">Real Time Value</h2>
-                <Tooltip className="ml-2">
-                    <div className="text-xs text-center whitespace-nowrap">Refresh every 2 seconds
-                    </div>
-                </Tooltip>
             </header>
             {/* Chart built with Chart.js 3 */}
             {/* Change the height attribute to adjust the chart height */}
